7509eefc9d0d381d81752e4ae2ddb8ce476aa67a
teraz_kubi <kubacichowicz13@wp.pl>
2024-03-19 20:58:22 +0100

    add title to navbar

--- zabkaKB/static/Scripts/main.js
+++ zabkaKB/static/Scripts/main.js
@@ -4,12 +4,21 @@ const addNoteButton = document.querySelector(".navBar-addButton")
 
 const searchInput = document.querySelector('#navBar-searchInput')
 
+const title = document.querySelector(".titleContainer")
+
 // https://sweetalert2.github.io/#configuration
 
 window.onload = async ()=>{
     const data = await getNotes()
     displayData(data?.notes, reversed=true)
 }
+
+title.addEventListener('click', () => {
+    searchInput.focus()
+    searchInput.value = ''
+    const inputEvent = new Event('input')
+    searchInput.dispatchEvent(inputEvent)
+})
 
 addNoteButton.addEventListener('click', async () => {
     await displayNoteEditor()
